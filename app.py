8ebc1ce2a94bb663fb6d2835713717aa85265563
Demesmaeker <morgane.demesmaeker@gmail.com>
2020-11-24 12:43:17 +0100

    jsonify the response + prettify documentation

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 from flask import Flask, request, jsonify
-from flask_restx import Api, Resource, abort
+from flask_restx import Api, Resource, abort, fields
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 
@@ -12,6 +12,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Fix the Flask-RESTx https error
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+resource_fields = api.model('Resource', {
+    'ZIP': fields.Integer,
+    'type_of_property' : fields.String,
+    'subtype_of_property' : fields.String,
+    'number_of_rooms' : fields.Integer,
+    'house_area' : fields.Integer,
+    'surface_of_the_land' : fields.Integer,
+    'number_of_facades' : fields.Integer,
+    'swimming_pool' : fields.Integer,
+    'state_of_the_building' : fields.String,
+    'road' : fields.String,
+    'number' : fields.Integer
+})
 
 
 @api.route('/status', methods=['GET'])
@@ -26,20 +40,12 @@ class status(Resource):
 
 
 @api.route('/predict', methods=['POST'])
-@api.doc(params={
-    'ZIP': 'Int : Postal Code',
-    'type_of_property' : 'house or apart',
-    'subtype_of_property' : 'house, apartment, villa, duplex, ground floor, penthouse, mixed use building, flat studio, mansion, exceptional property, town house, service flat, country cottage, bungalow, loft, chalet, farmhouse, triplex, manor house, kot, other property',
-    'house_area' : 'Int : m2',
-    'surface_of_the_land' : 'Int : m2',
-    'state_of_the_building' : 'New, ...'
-}, description='Enter these parameters to get a price prediction')
+@api.doc(body=resource_fields, description='Enter these parameters to get a price prediction')
 class predict(Resource):
     def post(self):
-        building = request.get_json()
-        number = randint(100000, 500000)
-        return number
-
+        infos = request.get_json()
+        response = {'prediction' : randint(100000, 500000)}
+        return jsonify(response)
 
 
 # Run server
